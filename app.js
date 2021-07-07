264cfdcf9c75161bb79c8623ade0caa61f54c522
Joshua Jameson-Wallis <jjw@Joshuas-MacBook-Air.local>
2021-07-07 12:46:05 +0100

    Updated Comments

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 // Fix spaces in id value when 3+ words 
 // ID names for inputs (to target in other event listeners?)
-// 
-// Edit btn click - disabled removed from name input (add b4 + class) 
+// Build label + checkbox - 'hide those who haven't responded' (sep func - run in form event listener - 'hideNonRespondees') | class (just add no toggle) - flex + space between | add only once? 
+// Checked input - if selected (pseduo class?) - label + outline turns light-blue (JS Class - toggle each time)
+// Edit btn click - disabled removed from name input (add b4) | edit btn text-content change to 'save' (when clicked - reverts back to edit + saves name input text-content as updated value by re-adding disabled attr) | 
+// Remove btn click - removes whole <li> parent 
 
 const form = document.querySelector('#registrar')
 const txtInput = document.querySelector('#name-submit')
