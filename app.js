4faeac00c8698d8c7011e014e2a8d155c8444634
Joshua Jameson-Wallis <jjw@Joshuas-MacBook-Air.local>
2021-07-07 14:50:16 +0100

    Hide Non-Respondees Function + Re-factored Array Push

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 // Fix spaces in id value when 3+ words 
 // ID names for inputs (to target in other event listeners?)
-// Build label + checkbox - 'hide those who haven't responded' (sep func - run in form event listener - 'hideNonRespondees') | class (just add no toggle) - flex + space between | add only once? 
 // Checked input - if selected (pseduo class?) - label + outline turns light-blue (JS Class - toggle each time)
 // Edit btn click - disabled removed from name input (add b4) | edit btn text-content change to 'save' (when clicked - reverts back to edit + saves name input text-content as updated value by re-adding disabled attr) | 
 // Remove btn click - removes whole <li> parent 
@@ -22,15 +21,14 @@ function appendListItems () {
 
     name.type = 'text'
     name.value = `${txtInput.value}`
-    arr.push(name)
 
     label.textContent = 'Confirmed'
     label.setAttribute("for", nameConverted)
-    arr.push(label)
 
     checked.type = 'checkbox'
     checked.id = nameConverted
-    arr.push(checked)
+
+    arr.push(name, label, checked)
 
     ul.appendChild(li)
 
@@ -53,13 +51,33 @@ function appendListItems () {
     txtInput.value = ''
 }
 
+function hideNonRespondees () {
+    const main = document.getElementById('main')
+    if (main.children.length <= 2) {
+        const arr = []
+        const label = document.createElement('label')
+        const checked = document.createElement('input')
+        label.textContent = `Hide those who haven't responded`
+        label.setAttribute('for', 'non-responded')
+        checked.type = 'checkbox'
+        checked.id = 'non-responded'
+        arr.push(label, checked)
+
+        for (let i = 0; i < arr.length; i++) {
+            main.insertBefore(arr[i], ul)
+        }
+    }
+}
+
+
 ////////// EVENT LISTENERS //////////
 
 form.addEventListener('submit', e => {
     e.preventDefault()
     appendListItems()
+    hideNonRespondees()
 })
 
-listItem.addEventListener('click', e => {
+// listItem.addEventListener('click', e => {
 
-})
+// })
